5958c1e05d9d4872a947b24fd20c478f25a2b845
geyser-digitalfemsa <139982172+geyser-digitalfemsa@users.noreply.github.com>
2023-08-03 18:51:20 -0600

    iteración 4: segregación de interfaz

--- printer.ts
+++ printer.ts
@@ -1,5 +1,8 @@
 export interface Printer {
   printContent(content: string): void;
+}
+
+export interface PrinterF {
   printFancyContent(content: string): void;
 }
 
@@ -9,6 +12,23 @@ export class SimplePrinter implements Printer {
   }
 }
 
-export class FancyPrinter implements Printer {
+export class FancyPrinter implements PrinterF {
   // TODO: Fill in the missing code to adhere to the Interface Segregation Principle
+  public printFancyContent(content: string): void {
+    console.log(`Printing: ${content}`);
+  }
 }
+
+// Example Usage:
+const simplePrinter: Printer = new SimplePrinter();
+const fancyPrinter: PrinterF = new FancyPrinter();
+
+simplePrinter.printContent("Hello, this is a simple printer."); // Output: Printing: Hello, this is a simple printer.
+fancyPrinter.printFancyContent("Hello, this is a fancy printer."); // Output: Fancy printing: Hello, this is a fancy printer.
+
+/**
+ * en este caso se separó la interfaz Printer en dos
+ * por así decirlo. la cual ahora es Printer y PrinterF,
+ * cada una con un método adecuado. Así cada clase 
+ * implementa una interfaz adecuada.
+ */
