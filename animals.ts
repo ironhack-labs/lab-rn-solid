01b962506b54b2430bc5432a679e6ab01aeb60bf
geyser-digitalfemsa <139982172+geyser-digitalfemsa@users.noreply.github.com>
2023-08-03 18:18:07 -0600

    iteración 3: Principio de sustitución de Liskov

--- animals.ts
+++ animals.ts
@@ -1,5 +1,20 @@
-export class Animal {
+/*
+Requirements:
+
+Complete the Animal class to ensure it follows the Liskov Substitution Principle. 
+The Animal class should have a method named getInfo that returns information about the animal.
+
+Complete the Dog class to ensure it follows the Liskov Substitution Principle. 
+The Dog class should implement the getInfo method to return information specific to a dog.
+
+Complete the Fish class to ensure it follows the Liskov Substitution Principle. 
+The Fish class should implement the getInfo method to return information specific to a fish.
+*/
+
+export abstract class Animal {
   // TODO: Fill in the missing code to adhere to the Liskov Substitution Principle
+
+  abstract getInfo(): string;
 }
 
 export class Dog extends Animal {
@@ -11,6 +26,9 @@ export class Dog extends Animal {
   }
 
   // TODO: Fill in the missing code to adhere to the Liskov Substitution Principle
+  public getInfo(): string {
+    return this.name;
+  }
 }
 
 export class Fish extends Animal {
@@ -22,9 +40,28 @@ export class Fish extends Animal {
   }
 
   // TODO: Fill in the missing code to adhere to the Liskov Substitution Principle
+  public getInfo(): string {
+    return this.type;
+  }
 }
 
 // Example Usage:
 export function printInfo(animal: Animal): void {
   console.log(`Info: ${animal.getInfo()}`);
 }
+
+const dog = new Dog("Buddy");
+const fish = new Fish("Goldfish");
+
+printInfo(dog); // Output: Info: Dog - Buddy
+printInfo(fish); // Output: Info: Fish - Goldfish
+
+/**
+ * Se tuvo que reafactorizar un poco la clase base,
+ * que ahora es abstracta. con ello podemos usar
+ * el parametro animal de tipo Animal y ejecutar 
+ * el metodo abstracto getInfo en la función printInfo,
+ * que da como resultado la información distintiva 
+ * del objeto animal ya que en cada subclase de
+ * implementa el método abstracto getInfo.
+ */
